f22943fb41ff8170bf24c4941f7a6c6be0dd29bb
naser3000 <naser.shokri@yahoo.com>
2019-08-07 21:19:50 +0430

    5th deploy on heroku

--- server.js
+++ server.js
@@ -1,44 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const path = require('path');
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+// Serve static files....
+app.use(express.static(__dirname + '/dist/chart-tagging'));
 
-
-
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
-
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
+// Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/chart-tagging/index.html'));
 });
 
-// git add .
-// git commit -m "first deploy"
-// git push heroku master
-
-//git remote -v           "current remote URL"
-//git remote add origin  https://git.heroku.com/boujan.git                         "remote to origin"
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
